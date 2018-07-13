fa061dc305f9f668fc3b54c264de5e7858d49567
Taylor Williams <taylor@thewilliams.net>
2018-07-13 21:57:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,25 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE students (id INT PRIMARY KEY, name TEXT, grade INT)")
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (name, grade)")
+  end
+
 end
